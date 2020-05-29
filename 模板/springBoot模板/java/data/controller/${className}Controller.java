27906473a328d3053a920390351fcb2f01959367
zhifa <zhifa128@vip.qq.com>
2020-05-29 16:29:07 +0800

    模板格式优化

--- 模板/springBoot模板/java/data/controller/${className}Controller.java
+++ 模板/springBoot模板/java/data/controller/${className}Controller.java
@@ -9,7 +9,6 @@ import com.bf.data.service.version.ApiVersion;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
