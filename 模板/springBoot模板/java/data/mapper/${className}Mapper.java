ce2ea9daddc9c4d89825bb15a93bf68782ae7f22
zhifa <zhifa128@vip.qq.com>
2020-06-03 14:57:48 +0800

    mapper 优化

--- 模板/springBoot模板/java/data/mapper/${className}Mapper.java
+++ 模板/springBoot模板/java/data/mapper/${className}Mapper.java
@@ -1,7 +1,6 @@
 package ${package}.mapper;
 
 import ${package}.entity.${className}Entity;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.stereotype.Repository;
 import com.github.pagehelper.Page;
 
