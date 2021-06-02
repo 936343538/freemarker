e265cc6b9db44437163a9486d32dfe027d009091
hongzf1024 <zhifa128@foxmail.com>
2021-06-02 16:42:41 +0800

    添加Criteria查询条件

--- 模板/euler_tld/java/${className?uncap_first}/service/${className}QueryParam.java
+++ 模板/euler_tld/java/${className?uncap_first}/service/${className}QueryParam.java
@@ -1,6 +1,7 @@
 package ${package}.${className?uncap_first}.service;
 
 import com.hsit.euler.base.criteria.Criteria;
+import ${package}.util.CommonUtil;
 
 /**
  * ${date}
@@ -8,8 +9,34 @@ import com.hsit.euler.base.criteria.Criteria;
  * ${table.comment!}
  */
 public class ${className}QueryParam {
+    <#list table.columns as column>
+    /**
+     * ${column.columnComment}<#if column.columnKey="1">[主键]</#if>
+     */
+    private ${column.columnType} ${column.columnName2};
+    </#list>
+
+    <#list table.columns as column>
+    public ${column.columnType} get${column.columnName2?cap_first}() {
+        return this.${column.columnName2};
+    }
+
+    public void set${column.columnName2?cap_first}(${column.columnType} ${column.columnName2}) {
+        this.${column.columnName2} = ${column.columnName2}<#if column.columnType="String">== null ? null : ${column.columnName2}.trim()</#if>;
+    }
+    </#list>
+
     public Criteria getCriteria() {
-        return Criteria.build();
+        return Criteria.build()
+        <#list table.columns as column>
+                <#if column.columnName="DATA_STATE">
+                <#break>
+                </#if>
+                <#if column.columnKey="0">
+                .equal("${column.columnName}", ${column.columnName2})
+                </#if>
+        </#list>
+                .equal("DATA_STATE", CommonUtil.DATE_STATE.NO);
     }
 }
 
